3067a11874a0f1e1f3e2f7133130e1d4712cb897
Italo de Oliveira Santos <italsant@amazon.com>
2022-04-28 14:03:50 -0300

    move secret-manager to it's own file, change cloud-run name, change references to the old SA, remove references of CloudSQL and some variables

--- infra/gcp/terraform/backend.tf
+++ infra/gcp/terraform/backend.tf
@@ -16,31 +16,13 @@ resource "google_cloud_run_service_iam_member" "backend_noauth" {
   member = "allUsers"
 }
 
-resource "google_secret_manager_secret" "github_token" {
-  provider = google-beta
-
-  secret_id = "github-token"
-
-  replication {
-    automatic = true
-  }
-}
-
-resource "google_secret_manager_secret_version" "github_token" {
-  provider = google-beta
-
-  secret = google_secret_manager_secret.github_token.id
-
-  secret_data = "asoidaoidaiodsd"
-}
-
 resource "google_cloud_run_service" "backend" {
-  name     = "titus-backend"
+  name     = "gh-issues-finder-backend"
   location = var.region
 
   template {
     spec {
-      service_account_name = google_service_account.backend.email
+      service_account_name = google_service_account.gh_issues_finder.email
       containers {
         image = "gcr.io/cloudrun/hello"
         ports {
@@ -55,36 +37,8 @@ resource "google_cloud_run_service" "backend" {
           value = "8080"
         }
         env {
-          name  = "IS_PRODUCTION"
-          value = var.is_production
-        }
-        env {
-          name  = "CORS_ORIGIN"
-          value = google_cloud_run_service.frontend.status.0.url
-        }
-        env {
-          name  = "PG_HOST"
-          value = "/cloudsql/${var.gcp_project_id}:${var.region}:${google_sql_database_instance.main.name}"
-        }
-        env {
-          name  = "PG_PORT"
-          value = "5432"
-        }
-        env {
-          name  = "PG_DB"
-          value = var.cloudsql_db_name
-        }
-        env {
-          name  = "PG_USER"
-          value = var.cloudsql_db_user
-        }
-        env {
           name  = "SECRETS_STRATEGY"
           value = "gcp"
-        }
-        env {
-          name  = "SECRETS_PG_PASS"
-          value = "${google_secret_manager_secret.db_password.id}/versions/latest"
         }
         env {
           name  = "JWT_SECRET"
@@ -104,7 +58,6 @@ resource "google_cloud_run_service" "backend" {
     metadata {
       annotations = {
         "autoscaling.knative.dev/maxScale"      = "1000"
-        "run.googleapis.com/cloudsql-instances" = "${var.gcp_project_id}:${var.region}:${google_sql_database_instance.main.name}"
         "run.googleapis.com/client-name"        = "cloud-console"
       }
     }
