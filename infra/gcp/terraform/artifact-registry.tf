1d937f57d84ca32978012fb2d6ce14d0088786b3
Italo de Oliveira Santos <italsant@amazon.com>
2022-05-04 15:48:13 -0300

    removing terraform files since we're not using it anymore

--- infra/gcp/terraform/artifact-registry.tf
+++
@@ -1,8 +0,0 @@
-resource "google_artifact_registry_repository" "main" {
-  provider = google-beta
-
-  location = var.region
-  repository_id = var.artifact_registry_repository_name
-  description = "Application Docker repository"
-  format = "DOCKER"
-}
